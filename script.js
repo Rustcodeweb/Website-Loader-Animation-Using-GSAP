8b4c71b1117a1e37c5262a7fce5beb8e41b3b6fc
Rustcode <rustcode95@gmail.com>
2021-07-10 10:56:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+new TimelineMax({repeat: -1})
+	.to(".ball-1", 0.6, { x:180 })
+	.to(".ball-1", 0.6, { x:0 });
+
+new TimelineMax({repeat: -1})
+	.to(".ball-2", 0.5, { x:180 })
+	.to(".ball-2", 0.5, { x:0 });
